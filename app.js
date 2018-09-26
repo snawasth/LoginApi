e574164e40ebcec55d6359b5a4582c13f9bba272
grimmhuntress <snehaa2104@gmail.com>
2018-09-27 00:10:52 +0530

    home commit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//To make use of Post, Put and related functions
+//Middleware to initialize body parser
+app.use(bodyParser.json());
+
 //The path for that particular Model
 Logindetail =require('./models/userdetails');
 
@@ -16,7 +20,7 @@ app.get('/', (req, res) => {
 	res.send('Please navigate to /api/logindetails to retrieve the information');
 });
 
-//To route to userdetails
+//To route to userdetails for retrieving data
 app.get('/api/logindetails', (req, res) => {
 	Logindetail.getlogindetails((err, logindetails) => {
 		if(err){
@@ -25,6 +29,48 @@ app.get('/api/logindetails', (req, res) => {
 		res.json(logindetails);
 	});
 }); 
+
+app.get('/api/logindetails/:_id', (req, res) => {
+	Logindetail.getlogindetailsbyId(req.params._id, (err, logindetails) => {
+		if(err){
+			throw err;
+		}
+		res.json(logindetails);
+	});
+});
+
+app.post('/api/books', (req, res) => {
+	var book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.put('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	var book = req.body;
+	Book.updateBook(id, book, {}, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.delete('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	Book.removeBook(id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+
 
 //The port where the API will be running
 app.listen(5000);
