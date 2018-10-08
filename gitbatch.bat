a7c5ce125275068bd2a304e30fbb802d1f363853
snawasth <sneha.awasthi@capgemini.com>
2018-10-08 19:34:41 +0530

    New Commit bitches

--- gitbatch.bat
+++ gitbatch.bat
@@ -1,7 +1,7 @@
 @echo off
 call git add .
 set /p id="Enter Commit Description: "\r\n
-echo "Wait....."
+echo Wait for Process to complete.....
 call git commit -m "%id%"
 REM call git push https://github.com/snawasth/cloudprint.git
 call git push origin master
